86df51176be6aa0b0c6da55f7388d7a63aff43bf
Thomas Korsnes <89935350+Korsfareren@users.noreply.github.com>
2022-10-17 09:19:22 +0200

    Initial Commit

---
+++ OdaClone/OdaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdaCloneApp.swift
+//  OdaClone
+//
+//  Created by Thomas Korsnes on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OdaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
